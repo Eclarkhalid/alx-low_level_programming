f0bca029d46c34e71cc664b98ad94ad0b72c260a
Eclarkhalid <eclarkhalid@gmail.com>
2023-03-21 16:20:47 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: Prints a quote using the puts function
+ *
+ *Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+/* Declare the function prototype for my_function */
+void my_function(void);
+#endif /* MAIN_H */
