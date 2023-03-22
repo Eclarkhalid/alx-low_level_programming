899715030bcb2a37f08f06c2dc2659ac73678ff0
Eclarkhalid <eclarkhalid@gmail.com>
2023-03-22 14:36:46 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,4 +10,10 @@ int _isalpha(int c);
 int _putchar(char c);
 int print_sign(int n);
 int _abs(int n);
+void print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 #endif /* MAIN_H */
