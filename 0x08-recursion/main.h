27c4efc99a9f0039eeabcdd284d81c515f9aae17
Eclarkhalid <eclarkhalid@gmail.com>
2023-04-05 10:55:54 -0700

    is palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,6 +11,8 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int check_prime(int n, int div);
 int is_palindrome(char *s);
+int check_palindrome(char *s, int start, int end);
+int _strlen_recursion(char *s);
 int wildcmp(char *s1, char *s2);
 #endif /* MAIN_H */
 
