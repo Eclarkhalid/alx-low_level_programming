b2cbd70594346d83337c83c70bdb63255da93eaa
Eclarkhalid <eclarkhalid@gmail.com>
2023-04-19 20:52:55 -0700

    sum them all

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,7 +7,8 @@
  * @size: number of elements in the array
  * @cmp: pointer to the function to use to compare values
  *
- * Return: index of the first element for which cmp doesn't return 0, or -1 if no element matches or if size <= 0
+ * Return: index of the first element for which cmp doesn't return 0,
+ * or -1 if no element matches or if size <= 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
-
 /**
  * main - entry point for the program
  * @argc: the number of arguments passed to the program
@@ -11,34 +10,27 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*op_func)(int, int);
-
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
-
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    op_func = get_op_func(argv[2]);
-
-    if (op_func == NULL || argv[2][1] != '\0')
-    {
-        printf("Error\n");
-        exit(99);
-    }
-
-    if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
-
-    result = op_func(num1, num2);
-    printf("%d\n", result);
-
-    return (0);
+int num1, num2, result;
+int (*op_func)(int, int);
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
 }
-
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+op_func = get_op_func(argv[2]);
+if (op_func == NULL || argv[2][1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
+if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+{
+printf("Error\n");
+exit(100);
+}
+result = op_func(num1, num2);
+printf("%d\n", result);
+return (0);
+}
